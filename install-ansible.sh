ab7d24e1f457f8bf2b5ba9f829b59e798305b71b
Derrick Ssemanda <derekssema@gmail.com>
2020-09-13 21:00:07 +0300

    Update install-ansible.sh

--- install-ansible.sh
+++ install-ansible.sh
@@ -4,5 +4,5 @@ apt-add-repository --yes --update ppa:ansible/ansible
 apt install -y ansible cowsay sshpass  python3 openssh-server sudo aptitude build-essential git python-dev
 apt upgrade -y
 ufw allow radius
-chmod 600 ./ansible.cfg
+#chmod 600 ./ansible.cfg
 echo -e "\n      *****Ansible Installation*****\n Result:============================================\n\n***Ansible has been successfully installed to this server***"
